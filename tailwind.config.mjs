f7e95bbdebffcb72c893cd7bb006ecb58a2fd11c
Miguel Ángel Durán <miduga@gmail.com>
2023-12-28 16:28:28 +0100

    Fix dark mode

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: 'class',
 	theme: {
 		extend: {},
 	},
