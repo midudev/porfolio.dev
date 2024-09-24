b36e33c62a7e20d5d78b8790fd36fc5b29282a41
Jose Escribano <jescribanomora@gmail.com>
2024-09-24 12:02:26 +0200

    Add Experience JSON

---
+++ src/data/Experience.ts
@@ -0,0 +1,32 @@
+export const Experience: Experience[] = [
+    {
+        date: "Oct 2022 – Feb 2024",
+        title: "Full-stack developer",
+        company: "Línea Gráfica",
+        description: [
+            "Development of fully customized modules/plug-ins in Prestashop and Shopify",
+            "ERP and API integrations of different services both on Laravel and Prestashop",
+            "Creation of dynamic and responsive user interfaces using React and CSS",
+        ],
+    },
+    {
+        date: "May 2021 – Oct 2022",
+        title: "Full-stack developer",
+        company: "Labelgrup Networks",
+        description: [
+            "Development of e-commerce websites with multi-store to operate in several countries using Prestashop",
+            "Creation of multiple API REST using Laravel",
+            "Development of responsive user interfaces using Bootstrap, Javascript and CSS",
+        ],
+    },
+    {
+        date: "Jul 2018 – May 2021",
+        title: "Full-stack developer",
+        company: "WebImpacto – The eCommerce Agency",
+        description: [
+            "Creation of fully customized web projects using Laravel under the MVC architecture",
+            "Development of more than 20 e-commerce projects",
+            "Experience working under the SCRUM methodology through JIRA",
+        ],
+    },
+];
