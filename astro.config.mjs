b954fb3d74f571cf7c732c24a38dcb01809fea84
David Burgos <dburgos834@gmail.com>
2025-02-14 22:17:12 -0600

    Adding project #1 data

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import robotsTxt from "astro-robots-txt"
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/'
+  site: 'https://davidburgos.vercel.app/'
 })
